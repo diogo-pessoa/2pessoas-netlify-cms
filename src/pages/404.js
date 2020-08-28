8dcfe6004b14bfaf271b46750a28b459ba58f5d3
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2020-08-28 20:55:54 +0100

    traduzindo páginas

--- src/pages/404.js
+++ src/pages/404.js
@@ -16,11 +16,11 @@ const NotFound = () => (
           fontSize: "128px",
           color: "var(--primary-color)"
         }}/>
-        <h1>Oops we did not expect that to happen</h1>
-        <p>Have you wondered into the unknow. Let us help you, Please take a look at below options</p>
+        <h1>Oops, desculpe Isso não era esperado</h1>
+        <p>Por favor dê uma olhada nos links abaixo para continuar navegando no site.</p>
       </header>
-      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
-      <Link to="/contact" className="button -outline">Report this <RiBugLine className="icon -right"/></Link>
+      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Página inicial</Link>
+      <Link to="/contact" className="button -outline">reporte o problema <RiBugLine className="icon -right"/></Link>
     </div>
   </Layout>
 )
